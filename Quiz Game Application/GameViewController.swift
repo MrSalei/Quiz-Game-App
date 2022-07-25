55a95240d77323751ed6793da78299b603d150ed
MrSalei <ilia74311@yandex.by>
2022-07-25 21:30:11 +0300

    start edits

--- Quiz Game Application/GameViewController.swift
+++ Quiz Game Application/GameViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class GameViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var gameModels = [Question]()
+    var gameModels = [Question](), questionIndex = 0, used: [Int] = []
     
+    @IBOutlet weak var progressSlider: UISlider!
     var currentQuestion: Question?
     
     @IBOutlet var label: UILabel!
@@ -21,7 +22,9 @@ class GameViewController: UIViewController, UITableViewDelegate, UITableViewData
         table.delegate = self
         table.dataSource = self
         setupQuestions()
-        configureUI(question: gameModels.first!)
+        questionIndex = Int.random(in: 0..<gameModels.count)
+        used.append(questionIndex)
+        configureUI(question: gameModels[questionIndex])
     }
     
     private func configureUI(question: Question) {
@@ -53,10 +56,51 @@ class GameViewController: UIViewController, UITableViewDelegate, UITableViewData
             Answer(text: "London", correct: true),
             Answer(text: "Prague", correct: false)
         ]))
+        gameModels.append(Question(text: "7 - 5 = ?", answers: [
+            Answer(text: "2", correct: true),
+            Answer(text: "3", correct: false),
+            Answer(text: "4", correct: false),
+            Answer(text: "5", correct: false)
+        ]))
+        gameModels.append(Question(text: "8 % 3 = ?", answers: [
+            Answer(text: "1", correct: false),
+            Answer(text: "2", correct: true),
+            Answer(text: "3", correct: false),
+            Answer(text: "4", correct: false)
+        ]))
+        gameModels.append(Question(text: "79 + 23 = ?", answers: [
+            Answer(text: "100", correct: false),
+            Answer(text: "101", correct: false),
+            Answer(text: "102", correct: true),
+            Answer(text: "103", correct: false)
+        ]))
+        gameModels.append(Question(text: "91 - 69 = ?", answers: [
+            Answer(text: "20", correct: false),
+            Answer(text: "21", correct: false),
+            Answer(text: "22", correct: true),
+            Answer(text: "23", correct: false)
+        ]))
+        gameModels.append(Question(text: "2 + 2 * 2 = ?", answers: [
+            Answer(text: "2", correct: false),
+            Answer(text: "4", correct: false),
+            Answer(text: "5", correct: false),
+            Answer(text: "6", correct: true)
+        ]))
+        gameModels.append(Question(text: "11 * 6 / 22 = ?", answers: [
+            Answer(text: "1", correct: false),
+            Answer(text: "2", correct: false),
+            Answer(text: "3", correct: true),
+            Answer(text: "4", correct: false)
+        ]))
+        gameModels.append(Question(text: "56 / 8 = ?", answers: [
+            Answer(text: "7", correct: true),
+            Answer(text: "8", correct: false),
+            Answer(text: "9", correct: false),
+            Answer(text: "10", correct: false)
+        ]))
     }
     
     //Table view functions
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         currentQuestion?.answers.count ?? 0
     }
@@ -74,17 +118,23 @@ class GameViewController: UIViewController, UITableViewDelegate, UITableViewData
         let answer = question.answers[indexPath.row]
         
         if checkAnswer(answer: answer, question: question) {
-            if let index = gameModels.firstIndex(where: { $0.text == question.text }) {
-                if index < gameModels.count - 1 {
-                    let nextQuestion = gameModels[index + 1]
-                    currentQuestion = nil
-                    configureUI(question: nextQuestion)
+            progressSlider.value += 10
+            if progressSlider.value == 100 {
+                let alert = UIAlertController(title: "Done", message: "You won", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
+                present(alert, animated: true)
+            }
+            else {
+                while true {
+                    questionIndex = Int.random(in: 0..<gameModels.count)
+                    if !used.contains(questionIndex) {
+                        break
+                    }
                 }
-                else {
-                    let alert = UIAlertController(title: "Done", message: "You won", preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
-                    present(alert, animated: true)
-                }
+                used.append(questionIndex)
+                let nextQuestion = gameModels[questionIndex]
+                currentQuestion = nil
+                configureUI(question: nextQuestion)
             }
         }
         else {
